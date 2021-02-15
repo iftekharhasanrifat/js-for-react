413f5b2a4dacf80d101ad51dcd69b5ab8dfad108
Iftekhar Hasan <iftekharhasanrifat@gmail.com>
2021-02-15 12:50:27 +0000

    second commit

---
+++ 1-core-concepts.js
@@ -0,0 +1,45 @@
+// variable let const
+
+let money = 20;
+money = 15;
+
+const name = "Rifat";
+
+const person = {
+    name: 'iftekhar',
+    age: '23'
+}
+
+const statement = `This person is ${person.name} and his age is ${person.age}`;
+
+// conditions
+if (condition) {
+
+} else {
+
+}
+
+// Loops 
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+
+}
+
+// array 
+
+const ages = [24, 35, 43];
+const friends = ["Rifat", "Hasan", "Sudipta"];
+const products = [{ name: "Laptop", price: 500 }, { name: "Tab", price: 300 }, { name: "mobile", price: 100 }]
+
+// functions /arrow functions
+
+function name(params) {
+
+}
+
+const add = (x, y) => x + y;
+const multiply = x => 5 * x;
+const doSome = (x, y) => {
+    const sum = x + y;
+    return sum * 10;
+}
